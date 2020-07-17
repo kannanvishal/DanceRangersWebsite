56e80ef5e7fdd4b84190962bb20ab9139d5dd479
Jayabalan <kannan.jayabalan@accenture.com>
2020-07-18 02:19:59 +0530

    Updates

---
+++ precache-manifest.8235e817b593ab2be4ca2e145d3ee998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703a41da2e346aa289c722e9c0b64f37",
+    "url": "/DanceRangersWebsite/index.html"
+  },
+  {
+    "revision": "c662f35fda78efee038d",
+    "url": "/DanceRangersWebsite/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c662f35fda78efee038d",
+    "url": "/DanceRangersWebsite/static/js/2.c7f20fc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DanceRangersWebsite/static/js/2.c7f20fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ad1dbcc9fb0839c4e8",
+    "url": "/DanceRangersWebsite/static/js/main.2f8dee2a.chunk.js"
+  },
+  {
+    "revision": "29fb414a220eedae82ba",
+    "url": "/DanceRangersWebsite/static/js/runtime-main.eca76139.js"
+  }
+]);
