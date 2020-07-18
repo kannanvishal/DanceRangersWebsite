27440e64787a27c34abc10862b9d98688720f4b2
Jayabalan <kannan.jayabalan@accenture.com>
2020-07-18 16:55:57 +0530

    Icons Navigation

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
-/* "predeploy": "yarn build",
-"deploy": "gh-pages -d build" */
+/* Deploy Setup
+npm uninstall gh-pages
+npm install --save gh-pages
+yarn add gh-pages
+create repository in Git hub
+git add .
+git commit -m "First Commit."
+git remote rm origin
+git remote add origin https://github.com/kannanvishal/DanceRangersWebsite.git - url in github code page
+git push -u origin master
+add Homepage in package.json paste the url in github settings->branch master - //"homepage": "https://kannanvishal.github.io/DanceRangersWebsite/",
+add the below comment in script
+"predeploy": "yarn build",
+"deploy": "gh-pages -d build"
+git add .
+git commit -m "Add GitHub Pages Config."
+git config --global credential.helper wincred
+yarn run deploy 
+*/
