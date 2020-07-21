d9a0404e5ba87493131fb53b3ab5c9ed2d0e86f4
Jayabalan <kannan.jayabalan@accenture.com>
2020-07-21 15:23:22 +0530

    Hash Route Config.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import UnderConstruction from "./components/UnderConstruction";
 import About from './components/About';
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect
