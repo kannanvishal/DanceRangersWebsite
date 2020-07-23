040e1fcdc54a48680a571fac41effed04294e918
Jayabalan <kannan.jayabalan@accenture.com>
2020-07-24 01:25:43 +0530

    Whatsapp number change.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ function Contact(){
              </section>
              <section className="logo">
                  <article className="contactHeader">Send us Message</article>
-                    <a href="https://wa.me/9514203698?text=I'm%20interested%20in%20your%20webiste"><span className="iconify contactLogo" data-icon="ion-logo-whatsapp" data-inline="false"></span></a>
+                    <a href="https://wa.me/919514203698?text=I'm%20interested%20in%20your%20webiste"><span className="iconify contactLogo" data-icon="ion-logo-whatsapp" data-inline="false"></span></a>
                     <a href="https://www.instagram.com/dancerangers/?hl=en"><span className="iconify contactLogo" data-icon="uil:instagram-alt" data-inline="false"></span></a>
                     <a href="https://www.facebook.com/dancerangers04/"><span className="iconify contactLogo" data-icon="ant-design:facebook-filled" data-inline="false"></span></a>
                     <a href="mailto:dancerangers04@gmail.com"><span className="iconify contactLogo" data-icon="carbon:email" data-inline="false"></span></a> 
