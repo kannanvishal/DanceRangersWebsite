71a748cb60e81bc95bf850f42535ddf085724e98
Jayabalan <kannan.jayabalan@accenture.com>
2020-09-08 12:58:29 +0530

    Image Modification.

--- src/datas/RangersData.js
+++ src/datas/RangersData.js
@@ -2,25 +2,25 @@ const RangersData = [
     {
     id : 1,
     name : "KANNAN",
-    imgURL : "https://scontent-ort2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/92243817_219996089061317_8475444975724551329_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=105&_nc_ohc=YNNLbopTqcIAX-oYmTg&oh=8762f782d26af6c1ab6c08386c043290&oe=5F1F2CC4",
+    imgURL : "../Images/92243817_219996089061317_8475444975724551329_n.jpg",
     description : "Professional dancer and choreograper"
 },
 {
     id : 2,
     name : "DEBNITA",
-    imgURL : "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/96259878_721202608690219_6372349321305295841_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=RdEM5VaMZWYAX8HQXr8&oh=30037eb8ba218334ed17019c75cc83a4&oe=5F2001D1",
+    imgURL : "../Images/72488618_269030300714977_8979317537044153818_n.jpg",
     description : "Professional dancer and choreograper"
 },
 {
     id : 3,
     name : "JAGADEEP",
-    imgURL : "https://scontent-lga3-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/26863266_882387291939370_3169974408940879872_n.jpg?_nc_ht=scontent-lga3-2.cdninstagram.com&_nc_cat=110&_nc_ohc=70PxwDaEBfMAX8qyF-1&oh=05bc182c2942201815a8f068e5bb5a32&oe=5F1EA350",
+    imgURL : "../Images/43630901_541279529652855_4597993428756900411_n.jpg",
     description : "Professional dancer and choreograper"
 },
 {
     id : 4,
     name : "KAYALVIZHI",
-    imgURL : "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80652160_122555009254480_5239418012101280751_n.jpg?_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=104&_nc_ohc=DW2-3wjxq1gAX_TvZ0I&oh=2488ea3e4b4c3758b680d4b79cbc5606&oe=5F20401F",
+    imgURL : "../Images/117366172_1047524955662128_3096244255587741291_n.jpg",
     description : "Professional dancer and choreograper"
 },
 
